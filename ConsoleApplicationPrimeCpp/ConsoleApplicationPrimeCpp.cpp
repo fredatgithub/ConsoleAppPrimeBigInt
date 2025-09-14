a877d8169f65b3d189f5b6487769c22e194d9689
fred <domaine75@gmail.com>
2025-09-14 02:00:06 +0200

    time how long it takes to compute a big int prime

--- ConsoleApplicationPrimeCpp/ConsoleApplicationPrimeCpp.cpp
+++ ConsoleApplicationPrimeCpp/ConsoleApplicationPrimeCpp.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "PrimeHelper.cpp"
+#include <chrono>
 using namespace std;
 
 int main()
@@ -16,15 +17,22 @@ int main()
       cout << nombre << " n'est pas premier" << endl;
     }
 
-    string grandNombre = "18446744073713715001";
-    cout << endl << "Test si " << grandNombre << " est premier..." << endl;
-    if (IsBigIntPrime(grandNombre))
+    string bigIntNumber = "18446744073713715001";
+    cout << endl << "Test si " << bigIntNumber << " est premier..." << endl;
+
+    auto start = std::chrono::high_resolution_clock::now();
+    bool result = IsBigIntPrime(bigIntNumber);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Temps écoulé : " << elapsed.count() << " secondes\n";
+    
+    if (IsBigIntPrime(bigIntNumber))
     {
-      cout << grandNombre << " est premier" << endl;
+      cout << bigIntNumber << " est premier" << endl;
     }
     else
     {
-      cout << grandNombre << " n'est pas premier" << endl;
+      cout << bigIntNumber << " n'est pas premier" << endl;
     }
     return 0;
 }
